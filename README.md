10fd71e3ddce4fea96a4564fdbbe210e11d1dfc1
Jongkook Choi <jongkook90@gmail.com>
2022-10-29 13:43:29 +0900

    dataloader

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # manga_panel_segmentation
-manga panel segmentation using unet
+TODO : manga panel segmentation using unet
+
+## TODO
+* train model
+* panel order detection
+
+### Reference
+
+> Matsui, Yusuke, Kota Ito, Yuji Aramaki, Azuma Fujimoto, Toru Ogawa, Toshihiko Yamasaki, and Kiyoharu Aizawa. "Sketch-based manga retrieval using manga109 dataset." Multimedia Tools and Applications 76, no. 20 (2017): 21811-21838.
+
+> Aizawa, Kiyoharu, Azuma Fujimoto, Atsushi Otsubo, Toru Ogawa, Yusuke Matsui, Koki Tsubota, and Hikaru Ikuta. "Building a manga dataset “manga109” with annotations for multimedia applications." IEEE MultiMedia 27, no. 2 (2020): 8-18.
+
+> Fujimoto, Azuma, Toru Ogawa, Kazuyoshi Yamamoto, Yusuke Matsui, Toshihiko Yamasaki, and Kiyoharu Aizawa. "Manga109 dataset and creation of metadata." In Proceedings of the 1st international workshop on comics analysis, processing and understanding, pp. 1-5. 2016.
---
+++ data_loader.py
@@ -0,0 +1,179 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+# Docker host : Ubuntu 22.04, 5.15.0-50-generic, Driver Version: 520.61.05    CUDA Version: 11.8
+# Docker image : nvcr.io/nvidia/tensorflow:22.04-tf2-py3
+
+import os
+import tensorflow as tf
+import tensorflow_addons as tfa
+import examples.tensorflow_examples
+from examples.tensorflow_examples.models.pix2pix import pix2pix
+import xml
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import tensorflow_addons as tfa
+import math
+et = xml.etree.ElementTree
+print(tf.__version__, tfa.__version__)
+# -
+
+# Reference : 
+# Manga109
+"""
+@article{mtap_matsui_2017,
+    author={Yusuke Matsui and Kota Ito and Yuji Aramaki and Azuma Fujimoto and Toru Ogawa and Toshihiko Yamasaki and Kiyoharu Aizawa},
+    title={Sketch-based Manga Retrieval using Manga109 Dataset},
+    journal={Multimedia Tools and Applications},
+    volume={76},
+    number={20},
+    pages={21811--21838},
+    doi={10.1007/s11042-016-4020-z},
+    year={2017}
+}
+
+@article{multimedia_aizawa_2020,
+    author={Kiyoharu Aizawa and Azuma Fujimoto and Atsushi Otsubo and Toru Ogawa and Yusuke Matsui and Koki Tsubota and Hikaru Ikuta},
+    title={Building a Manga Dataset ``Manga109'' with Annotations for Multimedia Applications},
+    journal={IEEE MultiMedia},
+    volume={27},
+    number={2},
+    pages={8--18},
+    doi={10.1109/mmul.2020.2987895},
+    year={2020}
+}
+"""
+
+
+class manga109_dataloader:
+    def __init__(self, skip_empty=True, path="Manga109s_released_2021_12_30"):
+        with open(f"{path}/books.txt", "r") as f:
+            self.books = [x.strip() for x in f.readlines()]
+        self.path_annotations = f"{path}/annotations/"
+        self.path_images = f"{path}/images/"
+        self.skip_empty = skip_empty
+    
+    @staticmethod
+    def make_mask(image_shape, frames):
+        np_mask = np.zeros(image_shape, dtype=np.uint8)
+        np_mask[:,:,2] = 255
+        y_siz, x_siz = np_mask.shape[0:2]
+        border_mask = np.zeros(np_mask.shape[0:2], dtype=bool)
+        inside_mask = np.zeros(np_mask.shape[0:2], dtype=bool)
+        r = 10
+        kernel = np.fromfunction(lambda x, y: ((x-r)**2 + (y-r)**2 <= r**2)*1, (2*r+1, 2*r+1), dtype=int).astype(np.uint8)
+
+        border_mask[:,:] = False
+        for frame in frames:
+            y0, y1, x0, x1 = int(frame["ymin"]), int(frame["ymax"]),int(frame["xmin"]), int(frame["xmax"])
+            y0, y1, x0, x1 = max(r,y0), min(y_siz-r,y1), max(r,x0), min(x_siz-r,x1)
+            if (y0+r < y1-r) and (x0+r < x1-r):
+                border_mask[y0:y1, x0:x1]=True
+                inside_mask[y0+r:y1-r, x0+r:x1-r]=True
+
+        np_mask[:,:,1] = 255 * (scipy.ndimage.binary_dilation(border_mask, kernel)^inside_mask)
+        np_mask[:,:,0] = 255 * inside_mask
+        np_mask[:,:,2] = np.where(np.sum(np_mask[:,:,[0,1]], axis=2),0,252)
+
+        return tf.convert_to_tensor(np_mask)
+
+    @staticmethod
+    def augment(image, mask, r=10):
+        max_angle = 5
+        border_px = r * 2
+        degrees = np.random.random(1)[0] * max_angle * 2 - max_angle
+        fill_value = np.random.random(1)[0] * 255
+        image = tfa.image.rotate(
+            image, 
+            angles=degrees * math.pi / 180, 
+            interpolation="bilinear",
+            fill_mode = 'constant',
+            fill_value = fill_value
+        )
+        mask = tfa.image.rotate(
+            mask, 
+            angles=degrees * math.pi / 180, 
+            interpolation="nearest",
+            fill_mode = 'constant',
+            fill_value = 0
+        )
+        # low frequency noise
+        background = tf.random.uniform(shape=[x//300 for x in image.shape[:2]]+[1], minval=0, maxval=255, dtype=tf.int32)
+        background = tf.image.resize(background, image.shape[0:2], method='bilinear',#method='gaussian',
+            preserve_aspect_ratio=False,
+            antialias=False,
+        )
+        # image
+        background = tf.repeat(background,3,axis=2)
+        image = tfa.image.blend(image, background, 0.3)
+        image = tf.dtypes.cast(image, tf.uint8)
+        image = tf.image.random_brightness(image, 0.2)
+        if np.random.random(1)[0] > 0.5:
+            image = tfa.image.gaussian_filter2d(image, (5,5), sigma = 5)
+        # mask
+        np_mask = mask.numpy()
+        np_mask[:,:,2] = np.where( np_mask.sum(axis=2) == 0 , 255 , np_mask[:,:,2] )
+        mask = tf.convert_to_tensor(np_mask)
+        # handle panel on border
+        np_border = np.ones(np_mask.shape[:2], dtype=bool)
+        np_border[border_px:np_mask.shape[0]-border_px, border_px:np_mask.shape[1]-border_px] = False
+        np_border = np.where(np_mask[:,:,0]==255 , np_border, False)
+        np_mask[:,:,1] = np.where( np_border , 255 , np_mask[:,:,1] )
+        np_mask[:,:,0] = np.where( np_border , 0 , np_mask[:,:,0] )
+        mask = tf.convert_to_tensor(np_mask)
+
+        return image, mask
+
+    def load_all(self):
+        for book in self.books:
+            with open(self.path_annotations+"/"+book+".xml", 'r') as f:
+                annotation = et.fromstring(f.read())
+            pages = annotation[1]
+            for page in pages:
+                image, mask = self.load(book, page)
+                if (image == None) or (mask==None):
+                    continue
+                yield image, mask
+    
+    def load(self, book, page):
+        pagenum = page.attrib["index"]
+        image_path = self.path_images+"/"+book+f"/{('000'+pagenum)[-3:]}.jpg"
+        image = tf.io.read_file(image_path)
+        image = tf.image.decode_jpeg(image, channels=3)
+        
+        frames = sorted([x.attrib for x in page if x.tag=="frame"], key=lambda x: x["id"], reverse=False)
+        if self.skip_empty and (len(frames) == 0):
+            return None, None
+        mask = self.make_mask(image.shape,frames)
+        image, mask = self.augment(image, mask)
+        
+        return image, mask
+
+
+if __name__ == "__main__":
+    loader = manga109_dataloader()
+    img_mask = loader.load_all()
+
+    for _ in range(3):
+        image, mask = next(img_mask)
+        plt.figure()
+        plt.imshow(image.numpy())
+        plt.figure()
+        plt.imshow(mask.numpy())
+
+
